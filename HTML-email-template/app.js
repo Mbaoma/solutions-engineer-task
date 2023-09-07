ca58c730c8b9623a53089766304db44b418e3fac
Mbaoma <mmaryraphaella@gmail.com>
2023-09-07 07:22:36 +0100

    task: add code for HTML email template

---
+++ HTML-email-template/app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Fetching data from the external API
+    axios.get('http://pet-shop.buckhill.com.hr/api/v1/orders') 
+        .then(response => {
+            const orders = response.data;
+            renderOrders(orders);
+        })
+        .catch(error => {
+            console.error('Error fetching orders:', error);
+        });
+});
+
+function renderOrders(orders) {
+    const source = document.getElementById('template').innerHTML;
+    const template = Handlebars.compile(source);
+    const renderedHtml = template(orders);
+
+    document.getElementById('content').innerHTML = renderedHtml;
+
+    document.querySelectorAll('.pay-now').forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const orderId = orders[index].purchaseId;
+            window.location.href = `/checkout.html?orderId=${orderId}`;  // Redirect to checkout page
+        });
+    });
+}
